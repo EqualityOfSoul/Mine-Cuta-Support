e9f18569d64407a289d44ce3052cbfa547510296
KohCuta <45093767+KohCuta@users.noreply.github.com>
2018-12-06 23:04:17 +0700

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -4,17 +4,17 @@ module.exports = client => {
   console.log(`${client.user.username} Ready to playing with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
   function randStatus() {
     let status = [
-      `Say m$help || m$invite`, `Mine Cuta`,
+      `Say $,help || $,invite`, `Mention Me @(Mine Cuta Support)`,
       `Discord.io//MineCuta`,
       `Total Server : 800 Server`,
       `Using Windows 10 Edition`,
-      `m$invite untuk invite Mine Cuta ke servermu`,
+      `$,invite untuk invite Mine Cuta ke servermu`,
       `Created by : Mine Cuta Youtube#9297`,
       `Pukul ${moment().tz("Asia/Makassar").format('LT')} WITA`,
       `Pukul ${moment().tz("Asia/Jakarta").format('LT')} WIB`,
       `Pukul ${moment().tz("Asia/Jayapura").format('LT')} WIT`];
     let rstatus = Math.floor(Math.random() * status.length);
-    client.user.setActivity(status[rstatus], { type: 'PLAYING' });
+    client.user.setActivity(status[rstatus], { type: 'Online' });
   };
 setInterval(randStatus, 9000);
   console.log(`${client.user.username} sukses online!`);
