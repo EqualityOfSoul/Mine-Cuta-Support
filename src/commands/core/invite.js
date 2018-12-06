e0c38a63ae0e3cc133123b62b34fb52cf5a2b33f
KohCuta <45093767+KohCuta@users.noreply.github.com>
2018-12-06 22:51:20 +0700

    Create invite.js

---
+++ src/commands/core/invite.js
@@ -0,0 +1,26 @@
+const { RichEmbed: SharifEmbed } = require('discord.js');
+
+exports.run = async (client, message, args) => {
+
+let embed = new SharifEmbed()
+      .setColor("RANDOM")
+      .setAuthor("Ingin Menginvite ~~Mine Cuta Support~~ Ke Server Mu?")
+      .setDescription("**[Klik Disini](https://discordapp.com/api/oauth2/authorize?client_id=513174081051557899&permissions=8&scope=bot)**")
+      .setFooter(`Requested By : ${message.author.username}`);
+      
+     message.channel.send(embed);
+     
+}
+
+exports.conf = {
+  aliases: ['invitebot'],
+  cooldown: "5"
+
+}
+
+exports.help = {
+  name: "invite",
+  description: 'Untuk Menginvite ~~Mine Cuta Support~~ Ke Server Mu',
+  usage: 'invite'
+
+}
