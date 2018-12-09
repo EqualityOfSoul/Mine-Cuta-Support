fc67ff9955b236e670e92394e98b6a39c1ff8c78
KohCuta <45093767+KohCuta@users.noreply.github.com>
2018-12-09 20:47:59 +0700

    Update invite.js

--- src/commands/core/invite.js
+++ src/commands/core/invite.js
@@ -4,8 +4,10 @@ exports.run = async (client, message, args) => {
 
 let embed = new SharifEmbed()
       .setColor("RANDOM")
-      .setAuthor("Ingin Menginvite ~~Mine Cuta Support~~ Ke Server Mu?")
+      .setAuthor("Ingin Menginvite Mine Cuta Support Ke Server Mu?")
       .setDescription("**[Klik Disini](https://discordapp.com/api/oauth2/authorize?client_id=513174081051557899&permissions=8&scope=bot)**")
+      .setAuthor("Support Bot Mine Cuta Support")
+      .setDescription("**[Klik Disini untuk Support Bot](https://discord.gg/nbVNMHb**")
       .setFooter(`Requested By : ${message.author.username}`);
       
      message.channel.send(embed);
