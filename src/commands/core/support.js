d701eb1faa804d689b02473f116ee771eb19843e
KohCuta <45093767+KohCuta@users.noreply.github.com>
2018-12-09 21:08:03 +0700

    Create support.js

---
+++ src/commands/core/support.js
@@ -0,0 +1,30 @@
+const { RichEmbed: SharifEmbed } = require('discord.js');
+
+exports.run = async (client, message, args) => {
+
+let embed = new SharifEmbed()
+      .setColor("RANDOM")
+      .setAuthor("Support Bot Mine Cuta Support")
+      .setDescription("**[Klik Disini]https://discord.gg/cRB4xN4)**")
+
+
+
+
+       .setFooter(`Requested By : ${message.author.username}`);
+      
+     message.channel.send(embed);
+     
+}
+
+exports.conf = {
+  aliases: ['supportbot'],
+  cooldown: "5"
+
+}
+
+exports.help = {
+  name: "support",
+  description: 'Untuk Support Bot ~Mine Cuta Support~',
+  usage: 'support'
+
+}
