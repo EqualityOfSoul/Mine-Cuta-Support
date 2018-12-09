cb20aafc51adb1dbd43235c42035b0166d59a0bc
KohCuta <45093767+KohCuta@users.noreply.github.com>
2018-12-09 21:27:38 +0700

    Update invite.js

--- src/commands/core/invite.js
+++ src/commands/core/invite.js
@@ -7,8 +7,8 @@ let embed = new SharifEmbed()
       .setAuthor("Ingin Menginvite Mine Cuta Support Ke Server Mu?")
       .setDescription("**[Klik Disini](https://discordapp.com/api/oauth2/authorize?client_id=513174081051557899&permissions=8&scope=bot)**")
     
-      .setAuthor("Support Bot")
-      .setDescription("**[Support Klick Disini](https://discord.gg/cRB4xN4)**")
+    
+
       .setFooter(`Requested By : ${message.author.username}`);             
 
 
