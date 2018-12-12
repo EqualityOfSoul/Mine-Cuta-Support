90df67da9f522c26461dca4e09c57ffb21dcff0d
KohCuta <45093767+KohCuta@users.noreply.github.com>
2018-12-12 21:05:56 +0700

    Create uptime.js

---
+++ src/commands/developer/uptime.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const http = require('http');
+const app = express();
+
+// 5 Minute Ping Times
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
